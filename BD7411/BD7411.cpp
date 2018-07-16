311ce180037b3405fa86af6e7d4e5eff657f0cab
jutava <jutava@hotmail.fi>
2018-07-16 13:22:16 +0300

    Add files via upload

---
+++ BD7411/BD7411.cpp
@@ -0,0 +1,44 @@
+/*****************************************************************************
+  BD7411.cpp
+
+ Copyright (c) 2018 ROHM Co.,Ltd.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+******************************************************************************/
+//#include <avr/pgmspace.h>
+#include <Arduino.h>
+#include "BD7411.h"
+
+BD7411::BD7411(void)
+{
+
+}
+
+void BD7411::init(int pin)
+{
+  _out_pin = pin;
+}
+
+int BD7411::readoutpin(void)
+{
+  int rc;
+  
+  rc = digitalRead(_out_pin);
+  return (rc);
+}
